d7c3a06c19010ed106a347a4ab7eec832304997f
Issamhj <issamhajiwork2022@gmail.com>
2023-09-16 00:06:11 +0100

    portfolio using html&css&js

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+
+const showMenu = (navToggle, navId) => {
+    const toggle=document.getElementById(navToggle),
+          nav=document.getElementById(navId);
+    if(toggle && nav){
+        toggle.addEventListener("click",()=>
+          {
+              nav.classList.toggle("show");
+          })
+    }
+     
+}
+showMenu("nav_toggle","nav_menu");
+
+const navlink = document.querySelectorAll(".nav-link");
+function actionlink(){
+    navlink.forEach(n => n.classList.remove("active"));
+    this.classList.add("active");
+
+    document.getElementById("nav_menu").classList.remove("show");
+}
+navlink.forEach(n => n.addEventListener("click",actionlink))
+
+const sombre=document.getElementById("icone_nav");
+const x=sombre.addEventListener("click",functionsombre);
+
+function functionsombre(){
+    if(document.body.style.backgroundColor == "white")
+    document.body.style.backgroundColor = "DimGray";
+    else 
+    document.body.style.backgroundColor = "white";
+}
